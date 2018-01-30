8312d5290f9b6e0ff86e47614441caa7672ee4f7
qb3 <quentinbertrand54@gmail.com>
2018-01-30 16:30:03 +0100

    corrected the filter error, add dirac and gaussian example

--- TP1.py
+++ TP1.py
@@ -7,8 +7,19 @@ hdul = fits.open('ngc2997.fits')
 hdul.info()
 data = hdul[0].data
 
+def getIndices(k, l, j, tailleFiltre, tailleImage):
+    listeIndiceLigne = [(k + 2**j * (i1 - tailleFiltre//2) )%tailleImage for i1 in range(tailleFiltre)]
+    listeIndiceColonne = [(l + 2**j * (i2 - tailleFiltre//2) )%tailleImage for i2 in range(tailleFiltre)]
+    return [listeIndiceLigne, listeIndiceColonne]
+
+def getIndicesMirrorConditions(k, l, j, tailleFiltre, tailleImage):
+    listeIndiceLigne = [(k + 2**j * (i1 - tailleFiltre//2) )%tailleImage *( (k + 2**j * (i1 - tailleFiltre//2) )//tailleImage%2 ==0 ) + (tailleImage-(k + 2**j * (i1 - tailleFiltre//2) )%tailleImage-1)*( (k + 2**j * (i1 - tailleFiltre//2) )//tailleImage%2 ==1 )   for i1 in range(tailleFiltre)]
+    listeIndiceColonne = [(l + 2**j * (i2 - tailleFiltre//2) )%tailleImage *( (l + 2**j * (i2 - tailleFiltre//2) )//tailleImage%2 ==0 ) + (tailleImage-(l + 2**j * (i2 - tailleFiltre//2) )%tailleImage-1)*( (l + 2**j * (i2 - tailleFiltre//2) )//tailleImage%2 ==1 )   for i2 in range(tailleFiltre)]
+    return [listeIndiceLigne, listeIndiceColonne]
+    
 #la fonction ci-dessous effectue la convolution 2D de l'image cOld avec le filtre h * h
 # h est un filtre 1D
+#ici la condition aux limites est périodique
 def getNewCj(j, cOld, h):
     tailleFiltre = h.shape[0]
     tailleImage = CijOld.shape[0]
@@ -19,8 +30,8 @@ def getNewCj(j, cOld, h):
         for l in range(tailleImage):
             #ici on extrait la matrice à partir de laquelle on calcule la convolution
             #on prend le reste de la division euclidienne, nous appliquons donc des conditions de bords périodiques
-            listeIndiceLigne = [(k + 2**j * (i1 - tailleFiltre//2) )%tailleImage for i1 in range(tailleFiltre)]
-            listeIndiceColonne = [(l + 2**j * (i2 - tailleFiltre//2) )%tailleImage for i2 in range(tailleFiltre)]
+            [listeIndiceLigne, listeIndiceColonne] = getIndices(k, l, j, tailleFiltre, tailleImage)
+            #[listeIndiceLigne, listeIndiceColonne] = getIndicesMirrorConditions(k, l, j, tailleFiltre, tailleImage)
             convol = cOld[listeIndiceLigne, :]
             convol = convol[:, listeIndiceColonne]        
             #là on applique l'opération de convolution avec les filre
@@ -30,25 +41,21 @@ def getNewCj(j, cOld, h):
             cNew[k,l] = np.sum(convol)
     return cNew
 
-#cette fonction "dialte" le filtre h d'un facteur 2
-def extendh(h):
-    l = len(h)
-    hNew = np.zeros(2*l-1)
-    for i in range(l):
-        hNew[2*i] = h[i]
-    return h
+##########################################
+#faire la convolition avec scipy
+#grad = sig.convolve2d(data, , boundary='symm', mode='same')
+
 
 #cette fonction implémente l'algorihme "A trous" foreward
 #cette fonction renvoie la liste (w0, w1, ..., wn, cn)
 def getCjWj(n, image, h):
     cOld = image
-    hOld = h
     res= list()
+    cNew = 0
     for j in range(n):
-        cNew = getNewCj(j, cOld, hOld)
-        wNew = cNew - cOld
+        cNew = getNewCj(j, cOld, h)
+        wNew = cOld - cNew
         cOld = cNew
-        hOld = extendh(hOld)
         res.append(wNew)
     res.append(cNew)
     return(res)
@@ -57,34 +64,61 @@ def getCjWj(n, image, h):
 def reconstruct(res):
     imReconst = res.pop()
     for im in res:
-        imReconst = imReconst - im
+        imReconst = imReconst + im
     return imReconst
+
 
 #########################################################################################
 #tests pour vérifier que tout marche
-#
-h = np.array([1/16, 1/4, 3/8, 1/4, 1/16])
-CijOld = data
+#on défini le filtre
+h = np.array([1, 4, 6, 4, 1])/16
+#test sur un dirac
 
-j = 3
+#test sur la galaxie
+CijOld = data
+j = 0
 getCjWj(j, data, h)
 
 n = 3
 res = getCjWj(n, data, h)
-
 for image in res:
     plt.figure()
-    plt.imshow(image)
-    plt.colorbar()
+    plt.imshow(image, cmap='gray')
+
+
+dirac = np.zeros((256, 256))
+dirac[255//2, 255//2] = 1
+n = 3
+res = getCjWj(n, dirac, h)
+for image in res:
+    plt.figure()
+    plt.imshow(image, cmap='gray')
+    print('moyenne = ',np.mean(image))
 
 imReconst=reconstruct(res)
+plt.figure()
+plt.imshow(imReconst, cmap='gray')
+st=reconstruct(res)
+plt.figure()
+plt.imshow(imReconst, cmap='gray')
+
+plt.figure()
+plt.imshow(data, cmap='gray')
+
+#######################################################################################
+#bruit gaussien
+gauss = np.random.normal(0, 1, (256, 256))
+plt.figure()
+plt.imshow(gauss, cmap='gray')
+n = 5
+res = getCjWj(n, gauss, h)
+for image in res:
+    plt.figure()
+    plt.imshow(image, cmap='gray')
+    print('ecart type = ', np.std(image) )
     
-plt.figure()
-plt.imshow(imReconst)
-plt.colorbar()
+#méthode automatique pour calculer le niveau du bruit : méthode de la médiane
 
-plt.figure()
-plt.imshow(data)
-plt.colorbar()
 
-#il reste à faire la question avec le dirac
+
+
