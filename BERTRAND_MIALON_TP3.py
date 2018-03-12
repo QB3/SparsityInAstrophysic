93a433f2d0d66297844c1088332291a53ae35aeb
qb3 <quentinbertrand54@gmail.com>
2018-03-12 15:16:47 +0100

    kept doing TP3, only one question left

---
+++ BERTRAND_MIALON_TP3.py
@@ -0,0 +1,166 @@
+import numpy as np
+import pyBSS as bss
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+#################################################################
+#whitening data
+def whitening(cov):
+    eigenvalues, U= np.linalg.eig(cov)
+    print("eigenvalues = ", eigenvalues)
+    SigmaMoins05 = np.diag(eigenvalues**(-0.5))
+    PI = np.dot(SigmaMoins05, U.T)
+    return PI
+
+m = 6
+t = 10
+X = np.random.rand(m,t)
+cov = np.dot(X, X.T)
+PI = whitening(cov)
+Xtilde = np.dot(PI, X)
+#here we have to be carefull if the eigenvalues are too small
+#there may have errors
+print(np.dot(Xtilde, Xtilde.T))
+
+###################################################################
+#Best rank approximation
+def getBestRankApprox(X, n):
+    P, D, Q = np.linalg.svd(X, full_matrices=False)
+    print("D =", D)
+    Dprime = np.concatenate([np.ones(n), np.zeros(D.shape[0]-n)])
+    print("n = ",n)
+    Dprime = D * Dprime
+    
+    a = P.shape[0]
+    b = Q.shape[0]
+    
+    middleMatrix = np.zeros((a, b))
+    for i in range(D.shape[0]):
+        middleMatrix[i,i] = Dprime[i]
+    
+    res = np.dot(P, middleMatrix)
+    res = np.dot(res, Q)
+    return res
+    
+rank =  np.linalg.matrix_rank(X)
+print("rank = ", rank)
+res = getBestRankApprox(X, rank-1)
+print("rank(bestApprox) =", np.linalg.matrix_rank(res))
+
+#############################################################
+#apply PCA to random mixture of gaussians
+#on génère et on plot les points
+X, A, S = bss.GenerateMixture(n = 3, m=5)
+plt.figure()
+plt.scatter(X[0,:], X[1,:])
+
+#attention à bien prendre la transposée dans la formule
+pca = PCA(n_components=3)
+pca.fit(X.T)
+print("explained variance ratio = ", pca.explained_variance_ratio_)  
+print("singulatr values", pca.singular_values_)  
+
+#Does PCA works in this case ?
+# ça veut dire quoi la PCA marche ? à checker
+
+#########################################################################################
+##########################################################################################
+# Independent component analysis
+
+#SType = 2 to generate unifrorm variables
+n = 2
+m = 2
+SType = 2
+X, A, S = bss.GenerateMixture(n = n, m=m, SType = SType)
+Aica, Sica = bss.Perform_FastICA(X, n)
+print("A.shape = ", A.shape)
+print("Aica.shape = ", Aica.shape)
+print("A.shape = ", S.shape)
+print("Sica.shape = ", Sica.shape)
+#j'obtiens un warning FastICA did not converge si je ne précise pas n_component = n dans la FICA
+print("ration Sica[1,:] / S[0,:] = ", Sica[1,:] / S[0,:])
+print("ration Sica[0,:] / S[1,:] = ", Sica[0,:] / S[1,:])
+#on observe que S et Sica sont presque identique à une permutation près et un facteur d'chelle 0.11
+print("ration Aica[1,:] / A[0,:] = ", A[:,0]/Aica[:,1]) 
+print("ration Aica[0,:] / A[1,:] = ", A[:,1]/Aica[:,0])
+#on observe que S et Sica sont presque identique à une permutation près et un facteur d'chelle 0.11
+
+####################################################################"
+#evaluate the performance whane the conditionning number of the mmixing matrix varies
+listCdA = [1,2,10,50,100] 
+nSample = 10000
+
+for CdA in listCdA:
+    counter = 0
+    for i in range(nSample):
+        X, A, S = bss.GenerateMixture(n = n, m=m, SType = SType, CdA=CdA)
+        Aica, Sica = bss.Perform_FastICA(X, n)
+        counter = counter + bss.Eval_BSS(A,S,Aica,Sica)
+    print("CdA = ", CdA, " : mean bss.Eval", counter/nSample)
+    
+
+####################################################################
+#ChandraSims
+
+#on charge les données
+from astropy.io import fits
+hdul = fits.open('AstroImages.fits')
+hdul.info()
+xStar = hdul[0].data
+plt.figure()
+plt.imshow(xStar[0,:,:], cmap ="gray")
+plt.figure()
+plt.imshow(xStar[1,:,:], cmap ="gray")
+plt.figure()
+plt.imshow(xStar[2,:,:], cmap ="gray")
+
+X = xStar.reshape((3,256*256))
+n= 8
+Aica, Sica = bss.Perform_FastICA(X, n)
+#on obtient un warning, le nombre de composant est trop large
+
+############################################################################################
+###########################################################################################
+#Sparse blind source separation
+
+#GMCA unif sources
+n = 2
+m = 2
+SType = 2
+X, A, S = bss.GenerateMixture(n = n, m=m, SType = SType)
+Agmca,Sgmca,PinvAgmca = bss.Perform_GMCA(X, n)
+print("bss.Eval = ", bss.Eval_BSS(A, S, Agmca, Sgmca))
+#Could you comment on the results ??
+
+#GMCA sparse sources
+n = 2
+m = 2
+SType = 3
+X, A, S = bss.GenerateMixture(n = n, m=m, SType = SType)
+Agmca,Sgmca,PinvAgmca = bss.Perform_GMCA(X, n)
+print("bss.Eval = ", bss.Eval_BSS(A, S, Agmca, Sgmca))
+#Could you comment on the results ??
+
+# on remarque que al reconstruction dans le cas sparese est bien meilleure que dans le cas uniforme
+
+################################
+#
+nSample = 1000
+listSNR = [40,30,20,10]
+for SNR in listSNR:
+    counterICA = 0
+    counterGMCA = 0
+    for i in range(nSample):
+        X, A, S = bss.GenerateMixture(n = n, m=m, SType = SType, noise_level = SNR)
+        Aica, Sica = bss.Perform_FastICA(X, n)
+        counterICA = counterICA + bss.Eval_BSS(A,S,Aica,Sica)
+        Agmca, Sgmca, PinvA = bss.Perform_GMCA(X, n)
+        counterGMCA = counterGMCA + bss.Eval_BSS(A,S,Agmca,Sgmca)        
+    print("SNR = ", SNR, " : mean bss.Eval ICA", counterICA/nSample)
+    print("SNR = ", SNR, " : mean bss.Eval GMCA", counterGMCA/nSample)
+
+
+
+
+
+
