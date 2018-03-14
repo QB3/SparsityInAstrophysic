741df15d909bcf7f620e671fc1a0a81776f269da
qb3 <quentinbertrand54@gmail.com>
2018-03-14 13:13:26 +0100

    palm implemented

--- BERTRAND_MIALON_TP3.py
+++ BERTRAND_MIALON_TP3.py
@@ -147,7 +147,7 @@ print("bss.Eval = ", bss.Eval_BSS(A, S, Agmca, Sgmca))
 
 ################################
 #
-nSample = 1000
+nSample = 100
 listSNR = [40,30,20,10]
 for SNR in listSNR:
     counterICA = 0
@@ -180,25 +180,51 @@ def getLip1(A):
 def getLip2(S):
     return np.linalg.norm(np.dot(S, S.T), ord = 'fro')
 
-def softThrd(x, gamma):
-    return np.sign(x) * np.maximum(np.abs(x) - gamma, 0)
-
 import scipy.io
 mat = scipy.io.loadmat('chandra.mat')
 
 Areal = mat['A0']
 Sreal = mat['S0']
 X = np.dot(Areal, Sreal)
-X = X.reshape(X.shape[0], int(np.sqrt(X.shape[1])), int(np.sqrt(X.shape[1])))
-
-dimA = Areal.shape
-A0 = np.zeros(dimA)
-dimS = Sreal.shape
-S0 = np.zeros(dimS)
 
 import Starlet2D as tp1
 
 
+def softThrd(x, gamma):
+    return np.sign(x) * np.maximum(np.abs(x) - gamma, 0)
+
+def Starlet_Forward2D_Multidim(S=0,h=[0.0625,0.25,0.375,0.25,0.0625],J=1,boption=3):
+    (n, t)= S.shape
+    tailleIm = int(np.sqrt(t))
+    #on reshape les sources en image pour appliquer Starlet 2D
+    newS = S.reshape((n, tailleIm, tailleIm))
+    cMultidim = np.zeros((n, tailleIm, tailleIm))
+    wMultidim = np.zeros((n, tailleIm, tailleIm, J))
+    for i in range(n):
+        cMultidim[i] , wMultidim[i,:,:,:]=  tp1.Starlet_Forward2D(newS[i, :, :], h,J,boption)
+    return cMultidim, wMultidim
+
+def Starlet_Backward2D_Multidim(cMultidim,wMultidim):
+    (n, tailleIm, tailleIm, J) = wMultidim.shape
+    SformatImage = np.zeros((n, tailleIm, tailleIm))
+    for i in range(n):
+        SformatImage[i,:,:] = tp1.Starlet_Backward2D(cMultidim[i], wMultidim[i,:,:,:])
+    SformatLigne = SformatImage.reshape((n, tailleIm**2))
+    return SformatLigne
+
+def projOnBall(A):
+    n, m = A.shape
+    res = np.zeros((n,m))
+    for i in range(m):
+        Ci = A[:,i]
+        norm = np.linalg.norm(Ci)
+        if (norm <= 1):
+            res[:,i] = Ci
+        else:
+            res[:,i] = Ci/norm
+    return res
+        
+        
 #attention il faut imposer des contraintes supplémentaires 
 #A \in O_b : chacune des colonnes de A est normalisée en norme L2
 #il faut donc rajouter un terme de contrainte : (d'où les 3 termes dans PALM)
@@ -207,13 +233,25 @@ def palm(nIter, X, A0, S0, gamma1 = 2, gamma2 = 2):
     A = cp.copy(A0)
     S = cp.copy(S0)
     for i in range(nIter):
-        ci = gamma1 * getLip2(S)
-        A = A - 1/ci * getGrad1(X, A, S)
-        di = gamma2 * getLip2(A)
-        S = S - 1/di * getGrad2(X, A, S)
-        #
-        S = softThrd(S, di)
+        ci = gamma2 * getLip2(S)
+        grad2 = getGrad2(X, A, S)
+        A = A - 1/ci * grad2
+        A = projOnBall(A)
+        di = gamma1 * getLip2(A)
+        S = S - 1/di * getGrad1(X, A, S)
+        cMultidim, wMultidim = Starlet_Forward2D_Multidim(S, J=2)
+        wMultidim = softThrd(wMultidim, di)
+        S = Starlet_Backward2D_Multidim( cMultidim, wMultidim)
         print("evaluation bss = ", bss.Eval_BSS(Areal, Sreal, A, S))
     return A, S
-    
-mat = scipy.io.loadmat('Noise_single_simulation.mat')
+
+
+dimA = Areal.shape
+A0 = np.random.normal(0,1,dimA)
+dimS = Sreal.shape
+S0 = np.random.normal(0,1, dimS)
+nIter = 100
+
+A, S = palm(nIter, X, A0, S0, gamma1=1, gamma2=1)
+A0=A
+S0= S
